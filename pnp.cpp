cf08a5ee27edd87041ff52eebf13e9c1ce82ee75
mmadhura1014 <96589703+mmadhura1014@users.noreply.github.com>
2021-12-31 18:38:05 +0530

    Add files via upload

---
+++ pnp.cpp
@@ -0,0 +1,130 @@
+/*PRIORITY NON PREEMPTIVE SCHEDULING ALGORITHMS*/
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+#include <string.h>
+using namespace std;
+
+struct process {
+    int pid;
+    int arrival_time;
+    int burst_time;
+    int priority;
+    int start_time;
+    int completion_time;
+    int turnaround_time;
+    int waiting_time;
+    int response_time;
+};
+
+int main() {
+
+    int n;
+    struct process p[100];
+    float avg_turnaround_time;
+    float avg_waiting_time;
+    float avg_response_time;
+    int total_turnaround_time = 0;
+    int total_waiting_time = 0;
+    int total_response_time = 0;
+    int total_idle_time = 0;
+    int is_completed[100];
+   memset(is_completed,0,sizeof(is_completed)); //memset function copies 0 to  entire is_completed array
+
+    cout << setprecision(2) << fixed;
+
+    cout<<"Enter the number of processes: ";
+    cin>>n;
+
+    for(int i = 0; i < n; i++) {
+        cout<<"Enter arrival time of process "<<i+1<<": ";
+        cin>>p[i].arrival_time;
+        cout<<"Enter burst time of process "<<i+1<<": ";
+        cin>>p[i].burst_time;
+        cout<<"Enter priority of the process "<<i+1<<": ";
+        cin>>p[i].priority;
+        p[i].pid = i+1;
+        cout<<endl;
+    }
+
+    int current_time = 0; //current time in the gantt chart
+    int completed = 0; //number of process completed its execution
+    int prev = 0; //keep track of idle time
+
+    while(completed != n) //the process is not completed its execution
+    {
+        int idx = -1;
+        int mn = 100;
+        for(int i = 0; i < n; i++) {
+            if(p[i].arrival_time <= current_time && is_completed[i] == 0) {
+                if(p[i].priority <  mn) { //lesser the priority number higher the priority
+                    mn = p[i].priority;
+                    idx = i;
+                }
+                if(p[i].priority == mn) {
+                    if(p[i].arrival_time < p[idx].arrival_time) {
+                        mn = p[i].priority;
+                        idx = i;
+                    }
+                }
+            }
+        }
+        if(idx != -1) {
+            p[idx].start_time = current_time;
+            p[idx].completion_time = p[idx].start_time + p[idx].burst_time;
+            p[idx].turnaround_time = p[idx].completion_time - p[idx].arrival_time;
+            p[idx].waiting_time = p[idx].turnaround_time - p[idx].burst_time;
+            p[idx].response_time = p[idx].start_time - p[idx].arrival_time;
+
+            total_turnaround_time += p[idx].turnaround_time;
+            total_waiting_time += p[idx].waiting_time;
+            total_response_time += p[idx].response_time;
+            total_idle_time += p[idx].start_time - prev;
+
+            is_completed[idx] = 1; //setting 1 to the process which completed its execution
+            completed++;
+            current_time = p[idx].completion_time; //last process completion time is current time now
+            prev = current_time;
+        }
+        else {
+            current_time++; //if no process arrives at particular time then increment current time( cpu is idle)
+        }
+
+    }
+
+
+
+    avg_turnaround_time = (float) total_turnaround_time / n;
+    avg_waiting_time = (float) total_waiting_time / n;
+    avg_response_time = (float) total_response_time / n;
+
+
+    cout<<endl<<endl;
+
+    cout<<"#P\t"<<"AT\t"<<"BT\t"<<"PRI\t"<<"ST\t"<<"CT\t"<<"TAT\t"<<"WT\t"<<"RT\t"<<"\n"<<endl;
+
+    for(int i = 0; i < n; i++) {
+        cout<<p[i].pid<<"\t"<<p[i].arrival_time<<"\t"<<p[i].burst_time<<"\t"<<p[i].priority<<"\t"<<p[i].start_time<<"\t"<<p[i].completion_time<<"\t"<<p[i].turnaround_time<<"\t"<<p[i].waiting_time<<"\t"<<p[i].response_time<<"\t"<<"\n"<<endl;
+    }
+    cout<<"Average Turnaround Time = "<<avg_turnaround_time<<endl;
+    cout<<"Average Waiting Time = "<<avg_waiting_time<<endl;
+    cout<<"Average Response Time = "<<avg_response_time<<endl; //since it is non preemptive response time is same as the waiting time
+}
+
+/*
+
+AT - Arrival Time of the process
+BT - Burst time of the process
+ST - Start time of the process
+CT - Completion time of the process
+TAT - Turnaround time of the process
+WT - Waiting time of the process
+RT - Response time of the process
+
+Formulas used:
+
+TAT = CT - AT
+WT = TAT - BT
+RT = ST - AT
+
+*/
